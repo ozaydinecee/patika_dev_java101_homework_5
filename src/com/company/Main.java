ced41842dd3357eddc51ebf9c3b750f799e5cb10
Ece Özaydın <77552205+ozaydinecee@users.noreply.github.com>
2022-11-02 10:33:17 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    
+       public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-	    System.out.println("Enter number:");
+        System.out.println("Enter number:");
         int number = input.nextInt();
-       /* for(int i = 1; i < number; i++){
-            if (i%2==0){
-                System.out.println(i+" is even");
-            }
 
-        }
-*/
-/*      int i =1;
-        while(i<=number){
-            if(i%2==0){
-                System.out.println(i+" is even");
-            }
-            i++;
-        }
-        */
         int i =0;
-        int sum=0;
+        double sum=0;
         while(i<=number){
             if(i%3==0 && i%4==0){
                 sum+=i;
@@ -32,7 +19,8 @@ public class Main {
             i++;
         }
         System.out.println("Summation of the numbers which are multiplies of 3 and 4. ");
-
-        System.out.println("Sum : "+ sum);
+        double average=sum/number;
+        System.out.println("Average : "+ average);
+    }
     }
 }
